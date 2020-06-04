1190d963b439af8cb6097e03f59f709c476786a0
Volodymyr <lapan40@ukr.net>
2020-06-04 13:52:55 +0300

    Fixed change output

--- lib/inserters/insert_money.rb
+++ lib/inserters/insert_money.rb
@@ -14,7 +14,7 @@ class InsertMoney
       if coin_value
         @inserted_money += coin_value
         if @inserted_money >= @required_sum
-          puts "Thanks, take your product and your change: #{@inserted_money - @required_sum}$"
+          puts "Thanks, take your product and your change: #{money_change}"
           return
         end
         status
@@ -31,12 +31,37 @@ class InsertMoney
     puts "Inserted: #{@inserted_money}$, Required: #{@required_sum}$"
   end
 
+  def money_change
+    res = ''
+    change = @inserted_money - @required_sum
+    change_left = change
+
+    Constants::AVAILABLE_CASH.each do |cash_val|
+      if change_left >= cash_val
+        res << cash_val.to_s + '$ '
+        change_left -= cash_val
+        redo
+      end
+    end
+
+    Constants::AVAILABLE_COINS.each do |cash_val|
+      if change_left >= cash_val
+        res << cash_val.to_s + 'c '
+        change_left -= cash_val
+        redo
+      end
+    end
+
+    res = '0$' if res.empty?
+    res
+  end
+
   def parse_coin(coin)
     # could be one regex but this just more readable
-    coin_match_data = coin.match(/\A(25|50)c\z/)
+    coin_match_data = coin.match(/\A(#{Constants::AVAILABLE_COINS.join('|')})c\z/)
     return ('0.' + coin_match_data[1]).to_f if coin_match_data
 
-    coin_match_data = coin.match(/\A(1|2|5)\$\z/)
+    coin_match_data = coin.match(/\A(#{Constants::AVAILABLE_CASH.join('|')})\$\z/)
     return coin_match_data[1].to_i if coin_match_data
   end
 end
--- lib/test_task/constants.rb
+++ lib/test_task/constants.rb
@@ -7,4 +7,7 @@ module Constants
     { id: 3, name: 'Sprite', price: 2, count: 1 },
     { id: 4, name: 'Lays', price: 2, count: 0 }
   ].map{|p| OpenStruct.new(p) }
+
+  AVAILABLE_COINS = [0.5, 0.25]
+  AVAILABLE_CASH = [ 5, 2, 1]
 end
